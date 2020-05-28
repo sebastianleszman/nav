08b4622abf25fef9a58dd83ea6786fa28a096d15
Sebastian Leszman <sebastianleszman@gmail.com>
2020-05-28 08:51:01 +0200

    another commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const menuToggleBtn = document.querySelector(".main-header__nav-toggle-btn");
 const mainNav = document.querySelector(".main-header__nav");
 
 function menuToggleClickHandler() {
-  mainNav.classList.toggle("menu-open");
+  mainNav.classList.toggle("hamburger--active");
 }
 
 menuToggleBtn.addEventListener("click", menuToggleClickHandler);
